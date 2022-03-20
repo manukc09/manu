fa9f6c9a346830bf86dc0517344d7650cc7e9623
Manu Chauhan <>
2022-03-20 06:55:02 +0000

    Changed the values of random numbers

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import random
-number = random.randint(1, 10)
-
+number = random.randint(2,12)
 player_name = input("Hello, What's your name?")
 number_of_guesses = 0
-print('okay! '+ player_name+ ' I am Guessing a number between 1 and 10:')
-
+print('okay! '+ player_name+ ' I am Guessing a number between 2 and 12:')
 while number_of_guesses < 5:
     guess = int(input())
     number_of_guesses += 1
@@ -18,5 +16,3 @@ if guess == number:
     print('You guessed the number in ' + str(number_of_guesses) + ' tries!')
 else:
     print('You did not guess the number, The number was ' + str(number))
-
-
